2a4e66cca256bed374df772f2ed84dd98166b268
Alex Moores <moores.alexd@gmail.com>
2017-07-19 17:14:44 -0400

    Cleaned up app.js, added comments

--- server/app.js
+++ server/app.js
@@ -5,15 +5,19 @@ const Promise = require('bluebird');
 const Sequelize = require('sequelize');
 const config = require('./config/config');
 
+
+// Initialize database
 var schema = new Sequelize(config.db.name, config.db.user, config.db.pass, {host: 'localhost', dialect: 'mysql'});
 db.syncTables(schema, false).then(() => {
   console.log('DB Initialized');
 });
 
+// Initialize Server
 const port = 8080;
 const app = express();
-app.use(express.static(path.join(__dirname, '../client')));
-require('./routes')(app, db);
+require('./routes')(app, db); // Import all middleware and routes
+
+// Launch Server
 app.listen(port, () => {
   console.log('Server running on port ', port)
 });
