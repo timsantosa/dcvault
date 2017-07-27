2021bc9a210fafbedca5a08d7ce4a1e6760cdea0
Alex Moores <moores.alexd@gmail.com>
2017-07-27 16:49:43 -0400

    working on footer

--- client/js/facilities.js
+++ client/js/facilities.js
@@ -48,29 +48,62 @@ const textValues = {
 <p class="content-text smaller-text">(NW corner of 22nd and East Capitol)</p> \
 <p class="content-text" style="font-weight: bold">What</p>\
 <p class="content-text smaller-text">Outdoor Pole Vault Training Center located on East Capitol street near the RFK Stadium. Used for training entry level through elite athletes, youth through adult, in group and private settings.</p>',
+};
+
+const locations = {
+  NCS: {lat: 38.927475, lng: -77.068477},
+  CUA: {lat: 38.942606, lng: -76.998025},
+  PREP: {lat: 39.032617, lng: -77.108889},
+  PG: {lat: 38.911269, lng: -76.866919},
+  BALT: {lat: 39.403604, lng: -76.626360},
+  DCV: {lat: 38.890134, lng: -76.976392},
+  CENTER: {lat: 39.168826, lng: -76.807406}
 }
+var map, markers;
 
 var current = 'DEFAULT';
 var textBox = document.getElementById('facilities-text-box');
 var facilities = Object.keys(textValues);
 facilities.shift(); // Removes "default" as a clickable option
 
-
 for (let i = 0; i < facilities.length; i++) {
   document.getElementById(facilities[i]).onclick = function () {
 
-    for (var j = 0; j < facilities.length; j++) { // Reset bg color of all
+    for (var j = 0; j < facilities.length; j++) { // Reset bg color of all, reset bouncing of all
       document.getElementById(facilities[j]).className = 'facility-tab';
+      markers[facilities[j]].setAnimation(null);
     }
 
     if (current === facilities[i]) {
       current = 'DEFAULT';
       textBox.innerHTML = textValues.DEFAULT;
-      this.className = 'facility-tab';
+      map.setCenter(locations.CENTER);
+      map.setZoom(9);
     } else {
       current = facilities[i];
       textBox.innerHTML = textValues[facilities[i]];
       this.className = 'facility-tab facility-tab-selected';
+      map.setCenter(locations[facilities[i]]);
+      map.setZoom(10);
+      markers[facilities[i]].setAnimation(google.maps.Animation.BOUNCE);
     }
+  }
+}
+
+window.initMap = function () {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: locations.CENTER,
+    scrollwheel: false,
+    mapTypeControl: false,
+    streetViewControl: false
+  });
+  markers = {
+    NCS: new google.maps.Marker({position: locations.NCS, map: map}),
+    CUA: new google.maps.Marker({position: locations.CUA, map: map}),
+    PREP: new google.maps.Marker({position: locations.PREP, map: map}),
+    PG: new google.maps.Marker({position: locations.PG, map: map}),
+    BALT: new google.maps.Marker({position: locations.BALT, map: map}),
+    DCV: new google.maps.Marker({position: locations.DCV, map: map})
   }
 }
