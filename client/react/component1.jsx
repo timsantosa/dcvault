6537eec043033aa4ca97c345079eb54e59fe8f76
Alex Moores <moores.alexd@gmail.com>
2017-07-27 19:39:56 -0400

    slight cleanup

--- client/react/component1.jsx
+++ client/react/component1.jsx
@@ -4,8 +4,8 @@ import {render} from 'react-dom';
 class TestComponent extends React.Component {
   render() { // All components have a render function in which you will return this 'HTML-like' syntax
     return (
-      <div style={{border: '2px solid black'}}>
-        <h2>This is a very basic React component</h2>
+      <div>
+        <p>Calendar Here</p>
       </div>
     );
   }
