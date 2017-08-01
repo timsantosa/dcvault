a721217521f8c433191fe8a2e8ae7a63cbcfca72
Alex Moores <moores.alexd@gmail.com>
2017-08-01 16:28:40 -0400

    added login close

--- client/react/component-directory.jsx
+++ client/react/component-directory.jsx
@@ -5,19 +5,3 @@ import Login from './login.jsx'
 
 render(<Calendar/>, document.getElementById('calendar'));
 render(<Login/>, document.getElementById('login'));
-
-var loginShown = false;
-
-document.getElementById('login-button').onclick = () => {
-  let loginDiv = document.getElementById('login');
-  loginShown = !loginShown;
-  if (loginShown) {
-    loginDiv.style.opacity = 0;
-    loginDiv.style.visibility = 'hidden';
-    console.log('hiding');
-  } else {
-    loginDiv.style.opacity = 1;
-    loginDiv.style.visibility = 'visible';
-    console.log('showing');
-  }
-}
--- client/react/login.jsx
+++ client/react/login.jsx
@@ -7,17 +7,31 @@ class Login extends React.Component {
       <div className='fade-bg'>
         <div className='login-modal'>
           <div className='row'>
-            <div className='col-xs-6'>
+            <div className='col-xs-4'>
               LOGIN
             </div>
-            <div className='col-xs-6'>
+            <div className='col-xs-4'>
               REGISTER
+            </div>
+            <div className='col-xs-4'>
+              <span class='glyphicon glyphicon-remove' id='login-menu-close'></span>
             </div>
           </div>
         </div>
       </div>
     );
   }
+}
+
+const loginDiv = document.getElementById('login');
+document.getElementById('login-button').onclick = () => {
+  loginDiv.style.opacity = 1;
+  loginDiv.style.visibility = 'visible';
+};
+
+document.getElementById('login-menu-close').onclick = () => {
+  loginDiv.style.opacity = 0;
+  loginDiv.style.visibility = 'hidden';
 }
 
 export default Login;
