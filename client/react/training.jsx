ebe5260a3848c59aebf949b897ba43e041a92559
Alex Moores <moores.alexd@gmail.com>
2017-08-25 18:32:54 -0400

    updated text descriptions

--- client/react/training.jsx
+++ client/react/training.jsx
@@ -9,17 +9,39 @@ class Training extends React.Component {
 
     this.state = {
       descriptions: {
-        beginner: '3-month course </br>\
-        Beginner, Level I athletes focus on developing primary phases of the vault. Class is held on weekends and includes static and dynamic stretching, biomechanically focused warm-up, primary phase drills and vaulting and equipment setup/takedown.',
-        intermediate: '3-month course </br>\
-        Intermediate, Level II athletes focus on mastering primary phases of the vault and basic introduction of secondary phases of the vault; Class is held on weekends and includes static and dynamic stretching, biomechanically focused warm-up, primary and secondary phase drills and vaulting, introductory speed and strength conditioning and equipment setup/takedown.',
-        emerging: 'Emerging Elite, Level III athletes focus on mastering secondary phases of the vault. Class is held on weekends and at multiple times during the week. Athletes participate in a 6-month comprehensive training program designed to produce maximum level of performance during the competitive season. Classes include static and dynamic stretching, biomechanically focused warm-up, secondary phase drills and vaulting, extensive speed and strength conditioning and equipment setup/take-down.',
-        elite: 'Elite, Level IV athletes focus on high level refinement of secondary phases of the vault, as well as mastering tertiary phases of the vault. Athletes participate in a 9-month comprehensive training program designed to produce two seasonal peaks in performance (indoor and outdoor championships) and to transition athletes to professional level training.',
-        professional: 'Professional, Level V athletes work in private session on a personalized, comprehensive, year-round training program. Technical focus is on mastering tertiary phases of the vault, with strong emphasis placed on high level refinement of energy transitions. Speed and strength development is customized for the individual athlete to produce peak performance during championship competitions. It incorporates recovery phases and non-traditional range of motion and biomechanical movements for stabilization, technical consistency and injury prevention.',
-        youthAdult: '​6-week course</br>\
-        Youth Classes (14 and under)</br>\
-          Adult Classes (21+)</br>\
+
+        beginner: '3-month course</br>\
+          ​Ages: All Ages</br>\
+          Location Options: DCV, BALT, PREP, PA​</br>\
+          ​Class Offerings: Athletes of All Ages can register for a beginner course held at one of the following training locations... DCV, BALT, PREP or PA.</br>\
+          Beginner, Level I athletes focus on developing primary phases of the vault. Class is held on weekends and includes static and dynamic stretching, biomechanically focused warm-up, primary phase drills and vaulting and equipment setup/takedown.',
+
+        intermediate: '3-month course</br>\
+          ​​Ages​: All Ages</br>\
+          ​Location Options: ​DCV, BALT, PREP​, ​PA</br>\
+          Intermediate, Level II athletes focus on mastering primary phases of the vault and basic introduction of secondary phases of the vault; Class is held on weekends and includes static and dynamic stretching, biomechanically focused warm-up, primary and secondary phase drills and vaulting, introductory speed and strength conditioning and equipment setup/takedown.',
+
+        emerging: '6-month course​</br>\
+          Ages: An invitation only group​</br>\
+          ​Locations: CUA, DCV​</br>\
+          Emerging Elite, Level III athletes focus on mastering secondary phases of the vault. Class is held on weekends and at multiple times during the week. Athletes participate in a 6-month comprehensive training program designed to produce maximum level of performance during the competitive season. Classes include static and dynamic stretching, biomechanically focused warm-up, secondary phase drills and vaulting, extensive speed and strength conditioning and equipment setup/take-down.',
+
+        elite: '9-month course​</br>\
+          Ages: An invitation only group​</br>\
+          ​Locations: CUA, DCV​</br>\
+          Elite, Level IV athletes focus on high level refinement of secondary phases of the vault, as well as mastering tertiary phases of the vault. Athletes participate in a 9-month comprehensive training program designed to produce two seasonal peaks in performance (indoor and outdoor championships) and to transition athletes to professional level training.',
+
+        professional: 'Year-round course​</br>\
+          Ages: An invitation only group​</br>\
+          ​Locations: CUA, DCV​, PG, NCS</br>\
+          Professional, Level V athletes work in private session on a personalized, comprehensive, year-round training program. Technical focus is on mastering tertiary phases of the vault, with strong emphasis placed on high level refinement of energy transitions. Speed and strength development is customized for the individual athlete to produce peak performance during championship competitions. It incorporates recovery phases and non-traditional range of motion and biomechanical movements for stabilization, technical consistency and injury prevention.',
+
+        youthAdult: '6-week course</br>\
+          ​Ages: ​Youth Classes (14 and under)</br>\
+          ​Ages: ​Adult Classes (21+)</br>\
+          ​Locations: DCV​</br>\
           Cl​​as​s​es specifically ​designed​ ​for​ beginner and intermediate youth and adults are offered at our DC Vault site​. Classes cover all primary phases of the vault, to include pole carry​/​approach, plant​/​takeoff, swing​/​turn.​',
+
         discounts: 'DC Vault offers a variety of discounts to needy athletes, local residents and service members, such as... </br>\
           </br>\
           10% - DC Residents </br>\
