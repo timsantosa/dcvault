7b66ff07fcebdb36d8cb2e6de47b38e310f4b717
admoores <moores.alexd@gmail.com>
2018-01-15 22:47:46 -0500

    more cleanup

--- server/lib/helpers.js
+++ server/lib/helpers.js
@@ -1,66 +1,68 @@
-'use strict';
+'use strict'
 
-const nodemailer = require('nodemailer');
-const config = require('../config/config');
-const path = require('path');
+/* eslint-disable no-multi-str */
+
+const nodemailer = require('nodemailer')
+const config = require('../config/config')
+const path = require('path')
 
 const transporter = nodemailer.createTransport({
-    host: config.email.server,
-    port: config.email.port,
-    secure: false, // secure:true for port 465, secure:false for port 587
-    auth: {
-        user: config.email.username,
-        pass: config.email.password
-    },
-    tls: {
-      ciphers: 'SSLv3'
-    }
-});
+  host: config.email.server,
+  port: config.email.port,
+  secure: false, // secure:true for port 465, secure:false for port 587
+  auth: {
+    user: config.email.username,
+    pass: config.email.password
+  },
+  tls: {
+    ciphers: 'SSLv3'
+  }
+})
 
 module.exports.sendCode = (code, email) => {
-  let link = config.server.domain + '/users/verify?code=' + code;
+  let link = config.server.domain + '/users/verify?code=' + code
   let mailOptions = {
-      from: '"DC Vault" <'+ config.email.username +'>', // sender address
-      subject: 'DC Vault New Account Verification', // Subject line
-      to: email,
-      text: 'Follow the Link to Verify your New Account: ' + link, // plain text body
-      html: '<p>Thank you for registering a DC Vault profile!</p>\
+    from: '"DC Vault" <' + config.email.username + '>', // sender address
+    subject: 'DC Vault New Account Verification', // Subject line
+    to: email,
+    text: 'Follow the Link to Verify your New Account: ' + link, // plain text body
+    html: '<p>Thank you for registering a DC Vault profile!</p>\
       <p>Your username is: ' + email + '</p>\
       <p>To verify your account, click the link below or copy/paste the following into your browser\'s URL bar: ' + link + '</p>\
       <p><a href="http://' + link + '">Click Here to Verify your Account</a></p>' // html body
-  };
+  }
 
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message %s sent to %s response: %s', info.messageId, email, info.response);
-  });
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message %s sent to %s response: %s', info.messageId, email, info.response)
+  })
 }
 
 module.exports.resetPass = (password, email) => {
   let mailOptions = {
-      from: '"DC Vault" <'+ config.email.username +'>', // sender address
-      subject: 'DC Vault Password Reset', // Subject line
-      to: email,
-      text: 'Your temporary password is as follows: ' + password + '\n Please change it at your earliest convenience through your account page.' // plain text body
-  };
+    from: '"DC Vault" <' + config.email.username + '>', // sender address
+    subject: 'DC Vault Password Reset', // Subject line
+    to: email,
+    text: 'Your temporary password is as follows: ' + password + '\n Please change it at your earliest convenience through your account page.' // plain text body
+  }
 
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message %s sent to %s response: %s', info.messageId, email, info.response);
-  });
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message %s sent to %s response: %s', info.messageId, email, info.response)
+  })
 }
 
 module.exports.sendConfirmationEmails = (email) => {
-    let mailOptions = {
-      from: '"DC Vault" <'+ config.email.username +'>', // sender address
-      subject: 'Welcome to DC Vault', // Subject line
-      to: email,
-      html: '<p>Thank you for registering with DC Vault. Please review the content below closely and let us know if you have any questions.</p>\
+  let mailOptions = {
+    from: '"DC Vault" <' + config.email.username + '>', // sender address
+    subject: 'Welcome to DC Vault', // Subject line
+    to: email,
+    html: '<p>Thank you for registering with DC Vault. Please review the content below closely and let us know if you have any questions.</p>\
       <h3>SCHEDULE</h3>\
       <p>Please refer to our online calendar for your training schedule. Your group dates, times and location for the upcoming training period are posted on this schedule. Any changes due to scheduling conflicts, weather or other unforeseen circumstances will be posted here as well. Please be sure to check the schedule prior to your training sessions at all times.</p> \
       <h3>WHAT TO EXPECT</h3>\
@@ -73,45 +75,44 @@ module.exports.sendConfirmationEmails = (email) => {
       <p>If you want to progress as quickly as possible, conditioning is very important. We\'ve attached our general team conditioning program. It\'s a 12 week program for our athletes to follow outside of scheduled practices. We strongly encourage every athlete to complete this workout in total. If you do, your progress will accelerate significantly.</p>\
       <p>If you have any questions that have not been answered, please email our administrative assistant, Lauren, at <a href="mailto:lrose@dcvault.org">LRose@DCVault.org</a>\
       <p>Again, thank you for joining us, and we look forward to seeing you soon.</p>',
-      attachments: [
-        {
-          filename: 'DC Vault Quarterly Pole Vault Conditioning L-I_L-II.pdf',
-          path: __dirname + '/../files/quarterly-conditioning.pdf'
-        },
-        {
-          filename: 'DC Vault Parking Pass.pdf',
-          path: __dirname + '/../files/parking-pass.pdf'
-        }
-      ]
-  };
+    attachments: [
+      {
+        filename: 'DC Vault Quarterly Pole Vault Conditioning L-I_L-II.pdf',
+        path: path.join(__dirname, '/../files/quarterly-conditioning.pdf')
+      },
+      {
+        filename: 'DC Vault Parking Pass.pdf',
+        path: path.join(__dirname, '/../files/parking-pass.pdf')
+      }
+    ]
+  }
 
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message %s sent to %s response: %s', info.messageId, email, info.response);
-  });
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message %s sent to %s response: %s', info.messageId, email, info.response)
+  })
 }
 
 module.exports.randString = (len) => {
-  len = len || 16;
-  let retVal = '';
+  len = len || 16
+  let retVal = ''
   let validChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV1234567890'
   for (var i = 0; i < len; i++) {
-    retVal += validChars.charAt(Math.floor(Math.random() * validChars.length));
+    retVal += validChars.charAt(Math.floor(Math.random() * validChars.length))
   }
-  return retVal;
+  return retVal
 }
 
-
 module.exports.sendWithReplyTo = (name, from, to, subject, text) => {
-    let mailOptions = {
-      from: '"' + name + '" <'+ config.email.username +'>', // sender address
-      replyTo: from,
-      subject: subject, // Subject line
-      to: to,
-      text: text // plain text body
-  };
+  let mailOptions = {
+    from: '"' + name + '" <' + config.email.username + '>', // sender address
+    replyTo: from,
+    subject: subject, // Subject line
+    to: to,
+    text: text // plain text body
+  }
 
-  return transporter.sendMail(mailOptions);
+  return transporter.sendMail(mailOptions)
 }
