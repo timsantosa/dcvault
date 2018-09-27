2cd9968c07dba04d06191a71c9121f4e2eae478a
admoores <moores.alexd@gmail.com>
2018-09-26 21:49:35 -0400

    minor style updates

--- client/react/calendar.jsx
+++ client/react/calendar.jsx
@@ -21,6 +21,7 @@ class Calendar extends React.Component {
           center: 'title',
           right: 'month,agendaWeek,agendaDay,listWeek'
         },
+        height: 850,
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         eventLimit: true, // allow "more" link when too many events
@@ -33,20 +34,7 @@ class Calendar extends React.Component {
     })
   }
 
-  toggleCalendar () {
-    if (this.state.showing) {
-      document.getElementById('google-embedded-calendar').style.display = 'none'
-      document.getElementById('toggle-calendar-button-text').innerHTML = 'Show Calendar'
-      this.setState({showing: false})
-    } else {
-      document.getElementById('google-embedded-calendar').style.display = 'block'
-      document.getElementById('toggle-calendar-button-text').innerHTML = 'Hide Calendar'
-      this.setState({showing: true})
-    }
-  }
-        // <iframe id='google-embedded-calendar' className='embed-responsive-item' src='https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showCalendars=0&amp;height=800&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=dcvaultt%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York' style={{borderWidth: '0', display: 'none'}} width='100%' height='400px' frameBorder='0' scrolling='no' />
-
-  render () { // All components have a render function in which you will return this 'HTML-like' syntax
+  render () {
     return (
       <div id='full-calendar' />
     )
