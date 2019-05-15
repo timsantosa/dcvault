790a591b3629f887dc22988023635cbc14cd68a8
Timothy Santosa <timmysantosa@gmail.com>
2019-05-15 18:26:54 -0400

    SQL DB Update

--- server/routes.js
+++ server/routes.js
@@ -460,7 +460,7 @@ module.exports = (app, db) => {
 
             db.tables.EventAthletes.create(athleteData)
             console.log("Athlete Created")
-            /*
+
             let purchaseInfo = req.body.purchaseInfo
             db.tables.EventPurchases.create({
                 waiverSignatory: purchaseInfo.agreement.name,
@@ -469,7 +469,7 @@ module.exports = (app, db) => {
                 payerId: purchaseInfo.event-payment.payerId,
                 athlete: event_athlete.fname + event_athlete.lname
             })
-            */
+
             console.log("purchase info added")
                 .catch((error) => {
                     res.status(500).send({ok: false, message: 'a db error has occurred', error: error})
