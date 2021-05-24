27a6982a9b3dd7a3da5cac7114a2165ebf1a00c8
Timothy Santosa <tsantosa@terpmail.umd.edu>
2021-05-24 09:21:10 -0400

    registration postpone

--- client/react/events.jsx
+++ client/react/events.jsx
@@ -38,7 +38,7 @@ class Events extends React.Component {
         <span className='event-block-details-header'>Registration</span>
         <ul className='event-block-details'>
           <li>Advanced Registration Only</li>
-          <li>Register <a href="https://dmvpvchamps.milesplit.com">Here</a></li>
+          <li>Registration Open Soon!</li>
         </ul>
         <span className='event-block-details-header'>Entry Fee</span>
         <ul className='event-block-details'>
@@ -91,7 +91,7 @@ class Events extends React.Component {
         <span className='event-block-details-header'>Registration</span>
         <ul className='event-block-details'>
           <li>Advanced Registration Only - Registration Open May 30th</li>
-          <li>Register <a href="https://dmvpvchamps.milesplit.com">Here</a></li>
+          <li>Registration Open Soon!/li>
         </ul>
         <span className='event-block-details-header'>Entry Fee</span>
         <ul className='event-block-details'>
