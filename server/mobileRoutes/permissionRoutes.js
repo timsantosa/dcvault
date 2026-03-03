a21d216fd76ae957e055f0e0971e48696cce6083
Ethan Dunne <dunne4ethan@gmail.com>
2026-03-03 18:44:22 -0500

    Gets are now protected by user or permission. CUD operations still protected by Permission

--- server/mobileRoutes/permissionRoutes.js
+++ server/mobileRoutes/permissionRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { checkPermission, checkOwnUserOrPermission } = require('../middlewares/mobileAuthMiddleware');
 const {
   getRolesForUser,
   addRoleToUser,
@@ -9,46 +10,52 @@ const {
   getPermissionsForRole,
   addPermissionToRole,
   removePermissionFromRole,
-  getAllUsersWithRole,
   createRole,
   deleteRole,
-  getAllRoles, 
+  getAllRoles,
   getAllPermissions
 } = require('../controllers/permissionsController');
+const { getAllUsersWithRole } = require('../controllers/usersController');
+
+const manageRoles = checkPermission('manage_roles');
 
 const permissionRoutes = (db) => {
   const router = express.Router();
 
-  // Pass the db to the controller functions
+  // User role: GET = own user or manage_roles, POST/DELETE = manage_roles
   router.route('/user/role')
-    .get((req, res) => getRolesForUser(req, res, db))
-    .post((req, res) => addRoleToUser(req, res, db))
-    .delete((req, res) => removeRoleFromUser(req, res, db));
+    .get((req, res, next) => checkOwnUserOrPermission(req, res, next, 'manage_roles'), (req, res) => getRolesForUser(req, res, db))
+    .post(manageRoles, (req, res) => addRoleToUser(req, res, db))
+    .delete(manageRoles, (req, res) => removeRoleFromUser(req, res, db));
 
+  // User permission: GET = own user or manage_roles, POST/DELETE = manage_roles
   router.route('/user/permission')
-    .get((req, res) => getPermissionsForUser(req, res, db))
-    .post((req, res) => addPermissionToUser(req, res, db))
-    .delete((req, res) => removePermissionFromUser(req, res, db));
+    .get((req, res, next) => checkOwnUserOrPermission(req, res, next, 'manage_roles'), (req, res) => getPermissionsForUser(req, res, db))
+    .post(manageRoles, (req, res) => addPermissionToUser(req, res, db))
+    .delete(manageRoles, (req, res) => removePermissionFromUser(req, res, db));
 
+  // Role permission: GET = any authenticated, POST/DELETE = manage_roles
   router.route('/role/permission')
-    .get((req, res) => getPermissionsForRole(req, res, db))
-    .post((req, res) => addPermissionToRole(req, res, db))
-    .delete((req, res) => removePermissionFromRole(req, res, db));
+    .get(manageRoles, (req, res) => getPermissionsForRole(req, res, db))
+    .post(manageRoles, (req, res) => addPermissionToRole(req, res, db))
+    .delete(manageRoles, (req, res) => removePermissionFromRole(req, res, db));
 
+  // List endpoints: any authenticated user
   router.route('/list')
     .get((req, res) => getAllPermissions(req, res, db));
 
   router.route('/roles')
     .get((req, res) => getAllRoles(req, res, db));
 
+  // Role CRUD: POST/DELETE = manage_roles
   router.route('/role')
-    .post((req, res) => createRole(req, res, db));
+    .post(manageRoles, (req, res) => createRole(req, res, db));
 
   router.route('/role/:roleId')
-    .delete((req, res) => deleteRole(req, res, db));
+    .delete(manageRoles, (req, res) => deleteRole(req, res, db));
 
   router.route('/role/:roleId/users')
-    .get((req, res) => getAllUsersWithRole(req, res, db));
+    .get(manageRoles, (req, res) => getAllUsersWithRole(req, res, db));
 
   return router;
 };
